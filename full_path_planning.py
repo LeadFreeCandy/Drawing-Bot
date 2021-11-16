d477fb7e08a767767f3518ba2f04ef4d2d230927
LeadFreeCandy <39067807+LeadFreeCandy@users.noreply.github.com>
2021-11-15 17:07:37 -0800

    oeuoe

--- full_path_planning.py
+++ full_path_planning.py
@@ -1,15 +1,44 @@
-seg = [(0,0), (1,0), (1,1)]
+import math
+import numpy as np
+
+seg = [(0,0), (1,0), (1,1), (2, 1)]
 
 def distance(x1, y1, x2, y2):
     return (((x2-x1) ** 2 + (y2 - y1) ** 2) ** .5)
 
+def parametric_arc(t: 0-1, starpos, endpos, radius):
+    pass
+
+def arc_points(startpos, endpos, num_poins):
+    pass
+
+
+
+
+def line_points(start_pos, end_pos, start_vel, end_vel, num_points):
+
+    points = []
+
+    if start_pos[0] == end_pos[0]:
+        x = start_pos[0]
+        for y in np.arange(start_pos[1], end_pos[1], (end_pos[1]-start_pos[1])/num_points):
+            points.append((x,y))
+    elif start_pos[1] == end_pos[1]:
+        y = start_pos[1]
+        for x in np.arange(start_pos[0], end_pos[0], (end_pos[0]-start_pos[0])/num_points):
+            points.append((x,y))
+    else:
+        for x,y in zip(np.arange(start_pos[0], end_pos[0], (end_pos[0]-start_pos[0])/num_points), np.arange(start_pos[1], end_pos[1], (end_pos[1]-start_pos[1])/num_points)):
+            points.append((x,y))
+
+    return points
 
 
 def calc_segment(seg, max_vel, max_accel, max_radius, frequency, john = "dumb"):
 
     # R = V^2/A
 
-    max_radius = 0
+    points = []
 
 
     velocity = 0
@@ -17,11 +46,42 @@ def calc_segment(seg, max_vel, max_accel, max_radius, frequency, john = "dumb"):
         ab_dist = distance(*a, *b)
         bc_dist = distance(*b, *c)
 
-        l = (max_accel * ab_dist - velocity ** 2) / (3 * max_accel) # math n shit
-        r = ab_dist - l
+        # l = (max_accel * ab_dist - velocity ** 2) / (3 * max_accel) # math n shit
+        r = ab_dist - (max_accel * ab_dist - velocity ** 2) / (3 * max_accel)
+        # vf = math.sqrt(velocity ** 2 + 2 * max_accel * l)
+
+        r = min(r, ab_dist/2, bc_dist/2, max_radius)
+        l = ab_dist-r
+
+        vf = min(math.sqrt(velocity ** 2 + 2 * max_accel * l), math.sqrt(max_accel * r))
+        assert velocity - max_accel *l <= vf, 'does not have time to slow'
 
 
-        radius = min(radius, ab_dist/2, bc_dist/2)
+
+        r2_max = bc_dist - r
+        vf2_max = math.sqrt(max_accel * r2_max)
+
+        if vf > vf2_max: #velocity limited
+            raise "uh maybe dis code do be needed doe"
+            vf = vf2_max
+            l = (vf ** 2 - velocity ** 2) / (2 * max_accel)
+            r = ab_dist - l
+
+
+        velocity = vf
+
+
+        # radius = min(radius, ab_dist/2, bc_dist/2)
+
+        print(f"{r = }, {l = }, {vf = }")
+
+        ratio = l/ab_dist
+        end_pos = (a[0] * (1-ratio) + b[0] * ratio, a[1] * (1-ratio) + b[1] * ratio)
+
+        points += line_points(a, end_pos, 0,0, 10)
+
+    return points
+
 
 def plot_path(points):
     import matplotlib.pyplot as plt
@@ -33,6 +93,7 @@ def plot_path(points):
 
 if __name__ == "__main__":
     # print(calc_segment(seg, radius=))
-    plot_path(seg)
+    points = calc_segment(seg, 1, 1, .1, 1)
+    plot_path(points)
 
 
