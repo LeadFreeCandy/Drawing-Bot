544233aa0c8f99e2ad2a390cf0d8084110729fa3
Samir <Samir.beall@gmail.com>
2021-11-18 11:08:25 -0800

    e

--- pieces.py
+++ pieces.py
@@ -1,4 +1,3 @@
-from cv2 import accumulate
 import numpy as np
 import math
 
@@ -12,6 +11,10 @@ def distance(x1, y1, x2, y2):
     return (((x2-x1) ** 2 + (y2 - y1) ** 2) ** .5)
 
 
+
+
+
+
 class Line():
 
     def __init__(self, start_pos, end_pos, start_vel = None, acceleration = None):
@@ -22,17 +25,45 @@ class Line():
         pass
     
     def __repr__(self):
-        return str(f"<Line from {self.start_pos} to {self.end_pos} {self.start_vel=} {self.acceleration=}>")
+
+        return str(f"<Line from {self.start_pos} to {self.end_pos} {self.start_vel=} {self.acceleration=} {self.end_vel=}>")
 
     def get_len(self):
         return distance(*self.start_pos, *self.end_pos)
 
-    def speed_delta(self):
-        return self.get_len() * self.acceleration
 
     @property
     def end_vel(self):
-        return self.start_vel + self.speed_delta()
+        if self.start_vel is not None:
+            return math.sqrt(self.start_vel ** 2 + 2 * self.acceleration * self.get_len())
+        else:
+            return None
+
+    def find_start_vel(self, accel, vf):
+        try:
+            return math.sqrt(vf ** 2 - 2 * accel * self.get_len())
+        except ValueError:
+            return 0
+    def find_accel(self, vf):
+        return (vf ** 2 - self.start_vel ** 2) / (2 * self.get_len())
+    def set_end_vel(self, vel, max_accel):
+        # if just modified accel, return None
+        # if modified accel and start_vel, return start_vel
+
+        # self.end_vel = vel
+        min_start_vel = self.find_start_vel(max_accel, vel) # THIS BROKE
+        # print(f"{max_accel=}")
+        # print(f"{vel=}")
+        # print(f"{self.end_vel=}")
+        # print(f"{self.start_vel=}")
+        # print(f"{min_start_vel=}")
+        if min_start_vel < vel:
+            self.acceleration = self.find_accel(vel)
+            return None
+        else:
+            self.start_vel = min_start_vel
+            self.acceleration = -max_accel
+            return self.start_vel
 
     def get_points_crude(self, num_points):
         points = []
